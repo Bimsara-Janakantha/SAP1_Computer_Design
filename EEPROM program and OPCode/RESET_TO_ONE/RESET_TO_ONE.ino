f015b4daf8d39cb4bfb0bbb54cbf91d0db4bceba
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-04-29 13:04:07 +0530

    Add files via upload

---
+++ EEPROM program and OPCode/RESET_TO_ONE/RESET_TO_ONE.ino
@@ -0,0 +1,100 @@
+/* This code is used to reset EEPROM to all 1 */
+
+int address[] = {12,11,10,9,8,7,6,5,4,3,2};
+int data[] = {22,24,26,28,30,32,34,36};
+int WE = 13;
+int dt = 10;
+
+void Write(){
+  digitalWrite(WE, HIGH);
+  delay(10);
+  digitalWrite(WE, LOW);
+  delay(2);
+  digitalWrite(WE, HIGH);
+}
+
+
+void setAdd(int value){
+  int i, Bit, binAddress[11];
+
+  Serial.print("Decimal Address: ");
+  Serial.print(value);
+  Serial.print("\t Binary Address: ");
+  
+  for(i=10; i>=0; i--){
+    Bit = (value & 1);
+    binAddress[i] = Bit;
+    value = value >> 1;
+  }
+
+  for(i=0; i<11; i++){
+    Serial.print(binAddress[i]);
+    Serial.print(" ");
+    digitalWrite(address[i], binAddress[i]);
+  }
+
+  Serial.print("  \t");
+
+  return;
+}
+
+
+void printData(){
+  int i, value;
+  Serial.print("Data Values: ");
+  
+  for(i=7; i>=0; i--){
+    value = digitalRead(data[i]);
+    Serial.print(value);
+    Serial.print(" ");
+    delay(10);
+  }
+  
+  Serial.print("\n");
+  
+  return;  
+}
+
+
+void setup() {
+  // Initializing Address Pins
+  for(int i=0; i<11; i++){
+    pinMode(address[i], OUTPUT);
+  }
+
+
+  // Initializing Data Pins
+  for(int i=0; i<8; i++){
+    pinMode(data[i], OUTPUT);
+  }
+  
+  // ReadOnly
+  pinMode(WE, OUTPUT);
+  digitalWrite(WE, 1);
+  
+  Serial.begin(9600);
+  Serial.print("Serial Monitor Starts\n\n");
+  Serial.print("READING DATA FROM EEPROM 28C16\n");
+
+  for(int i=0; i<2048; i++){
+    setAdd(i);
+    Serial.println();
+    delay(2);
+    for(int i=0; i<8; i++){
+      digitalWrite(data[i], 1);
+      delay(dt);
+      Write();
+      delay(dt);
+    }
+  }
+
+  Serial.println("Done");
+
+  //setAdd(66);
+  //Write();
+
+}
+
+void loop() {
+  
+}
